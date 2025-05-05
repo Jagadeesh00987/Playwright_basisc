a8e25e679815de8e77cbbf87c392403fc1b4551e
jagadeesh <jagadeesh@cloudrevelinnovation.com>
2025-05-05 16:20:07 +0530

    Windowshadled

--- tests/windowshandle.spec.js
+++ tests/windowshandle.spec.js
@@ -5,19 +5,35 @@ const context=await browser.newContext()
 const page= await context.newPage()
 
 await page.goto("https://www.tutorialspoint.com/selenium/practice/text-box.php")
-const newPage =await Promise.all(
+
+const [newPage] =await Promise.all(
 
 [
 context.waitForEvent('page'),
-await page.locator(".accordion-button collapsed"),
-await page.locator("//a[contains(@href,'browser')]").click(),
-await page.locator("button[title='New Tab']").click()
+page.getByRole('button', { name: 'Alerts, Frames & Windows' }).click(),
+page.getByRole('link', { name: 'Browser Windows' }).click(),
+//const page1Promise = page.waitForEvent('popup');
+page.getByRole('button', { name: 'New Tab' }).click()
 
 
 ]
 
 )
+await newPage.waitForTimeout(3000)
+const URL=await newPage.url()
+console.log("url is: "+ URL);
+expect(URL).toContain("https://www.tutorialspoint.com/selenium/practice/new-tab-sample.php")
+await newPage.waitForTimeout(4000)
+
+await newPage.close()
+
 await page.waitForTimeout(3000)
+const parentPage=await page.url()
+console.log("parentUrl is: "+ parentPage);
+expect(parentPage).toContain("https://www.tutorialspoint.com/selenium/practice/browser-windows.php")
+
+
+
 
 
 })
